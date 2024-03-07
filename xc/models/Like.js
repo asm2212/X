f976ffebf3f0ecf6ff09f5f9ca264bc9a053c73c
asm2212 <asmareadmasu0@gmail.com>
2024-03-08 00:39:40 +0300

    like

--- xc/models/Like.js
+++ xc/models/Like.js
@@ -1,12 +1,12 @@
-// import mongoose, {model, models, Schema} from "mongoose";
+import mongoose, {model, models, Schema} from "mongoose";
 
-// const LikeSchema = new Schema({
-//   author: {type:mongoose.Types.ObjectId, ref: 'User'},
-//   post: {type:mongoose.Types.ObjectId, ref: 'Post'},
-// }, {
-//   timestamps: true,
-// });
+const LikeSchema = new Schema({
+  author: {type:mongoose.Types.ObjectId, ref: 'User'},
+  post: {type:mongoose.Types.ObjectId, ref: 'Post'},
+}, {
+  timestamps: true,
+});
 
-// const Like = models?.Like || model('Like', LikeSchema);
+const Like = models?.Like || model('Like', LikeSchema);
 
-// export default Like;
+export default Like;
