5dd0db1733e73290e265637c4a12c0dc0437c146
asm2212 <asmareadmasu0@gmail.com>
2024-03-08 09:58:07 +0300

    id

--- xc/pages/[username]/status/[id].js
+++ xc/pages/[username]/status/[id].js
@@ -1,39 +1,55 @@
-import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
-import axios from "axios";
-import PostContent from "../../../components/PostContent";
-import Layout from "../../../components/Layout";
-import Link from "next/link";
-import useUserInfo from "../../../hooks/useUserInfo";
-import PostForm from "../../../components/PostForm";
-import TopNavLink from "../../../components/TopNavLink";
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import PostContent from '../../../components/PostContent';
+import Layout from '../../../components/Layout';
+import Link from 'next/link';
+import useUserInfo from '../../../hooks/useUserInfo';
+import PostForm from '../../../components/PostForm';
+import TopNavLink from '../../../components/TopNavLink';
 
 export default function PostPage() {
   const router = useRouter();
-  const {id} = router.query;
-  const [post,setPost] = useState();
-  const [replies,setReplies] = useState([]);
-  const [repliesLikedByMe,setRepliesLikedByMe] = useState([]);
-  const {userInfo} = useUserInfo();
+  const { id } = router.query;
+  const [post, setPost] = useState();
+  const [replies, setReplies] = useState([]);
+  const [repliesLikedByMe, setRepliesLikedByMe] = useState([]);
+  const { userInfo } = useUserInfo();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  function fetchData() {
-    axios.get('/api/posts?id='+id)
-      .then(response => {
-        setPost(response.data.post);
-      });
-    axios.get('/api/posts?parent='+id)
-      .then(response => {
-        setReplies(response.data.posts);
-        setRepliesLikedByMe(response.data.idsLikedByMe);
-      })
+  async function fetchData() {
+    try {
+      const [postResponse, repliesResponse] = await axios.all([
+        axios.get('/api/posts?id=' + id),
+        axios.get('/api/posts?parent=' + id),
+      ]);
+
+      setPost(postResponse.data.post);
+      setReplies(repliesResponse.data.posts);
+      setRepliesLikedByMe(repliesResponse.data.idsLikedByMe);
+      setIsLoading(false);
+    } catch (error) {
+      setError('Error fetching data');
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
     if (!id) {
       return;
     }
+
     fetchData();
   }, [id]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <Layout>
@@ -44,8 +60,10 @@ export default function PostPage() {
             <div className="pb-1">
               <PostContent {...post.parent} />
               <div className="ml-5 h-12 relative">
-                <div className="h-20 border-l-2 border-twitterBorder absolute -top-5"
-                     style={{marginLeft:'2px'}}></div>
+                <div
+                  className="h-20 border-l-2 border-twitterBorder absolute -top-5"
+                  style={{ marginLeft: '2px' }}
+                ></div>
               </div>
             </div>
           )}
@@ -56,17 +74,24 @@ export default function PostPage() {
       )}
       {!!userInfo && (
         <div className="border-t border-twitterBorder py-5">
-          <PostForm onPost={fetchData}
-                    parent={id}
-                    compact placeholder={'Tweet your reply'} />
+          <PostForm
+            onPost={fetchData}
+            parent={id}
+            compact
+            placeholder={'Tweet your reply'}
+          />
         </div>
       )}
       <div className="">
-        {replies.length > 0 && replies.map(reply => (
-          <div className="p-5 border-t border-twitterBorder" key={reply._id}>
-            <PostContent {...reply} likedByMe={repliesLikedByMe.includes(reply._id)} />
-          </div>
-        ))}
+        {replies.length > 0 &&
+          replies.map((reply) => (
+            <div className="p-5 border-t border-twitterBorder" key={reply._id}>
+              <PostContent
+                {...reply}
+                likedByMe={repliesLikedByMe.includes(reply._id)}
+              />
+            </div>
+          ))}
       </div>
     </Layout>
   );
